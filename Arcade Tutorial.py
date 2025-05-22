2a5224b84a486a054653a0a467c885a69a1db532
idotic-kid <em21236@my.westlake.school.nz>
2025-05-22 12:00:42 +1200

    Commit 22/05

--- Arcade Tutorial.py
+++ Arcade Tutorial.py
@@ -4,13 +4,14 @@ Platformer Game
 python -m arcade.examples.platform_tutorial.02_draw_sprites
 """
 import arcade
+from random import randint
 import math
 
 # Constants
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 WINDOW_TITLE = "slop game"
-PLAYER_MOVEMENT_SPEED = 10
+PLAYER_MOVEMENT_SPEED = 5
 TILE_SCALING = 0.5
 
 
@@ -23,9 +24,19 @@ class GameView(arcade.Window):
 
         # Call the parent class and set up the window
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+        self.highscore = 0
 
-        # Variable to hold our texture for our player
+
+        self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+
+
+    def setup(self):
+        """Set up the game here. Call this function to restart the game."""
+                # Variable to hold our texture for our player
         self.player_texture = arcade.load_texture(
+            "slpobat.png"
+        )
+        self.ball_texture = arcade.load_texture(
             "angry bvir.png"
         )
 
@@ -34,82 +45,91 @@ class GameView(arcade.Window):
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 128
         self.player_sprite.angle = 45
+        self.player_sprite.change_angle = 2
+
+        # variable for slop ball
+        self.ball = arcade.Sprite(self.ball_texture)
+        self.ball.center_x = 300
+        self.ball.center_y = 200
+        self.ball.change_x = PLAYER_MOVEMENT_SPEED
+        self.ball.change_y = PLAYER_MOVEMENT_SPEED
+
 
         # SpriteList for our player
         self.player_list = arcade.SpriteList()
         self.player_list.append(self.player_sprite)
-
-        # SpriteList for our boxes and ground
-        # Putting our ground and box Sprites in the same SpriteList
-        # will make it easier to perform collision detection against
-        # them later on. Setting the spatial hash to True will make
-        # collision detection much faster if the objects in this
-        # SpriteList do not move.
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
-
-        # Create the ground
-        # This shows using a loop to place multiple sprites horizontally
-        for x in range(0, 1300, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", scale=TILE_SCALING)
-            wall.center_x = x
-            wall.center_y = WINDOW_HEIGHT
-            wall.angle = 180
-            self.wall_list.append(wall)
-
-        for y in range(0, WINDOW_HEIGHT, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", scale=TILE_SCALING)
-            wall.center_x = 0
-            wall.center_y = y
-            wall.angle = 90
-            self.wall_list.append(wall)
-
-            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", scale=TILE_SCALING)
-            wall.center_x = WINDOW_WIDTH
-            wall.center_y = y
-            wall.angle = 270
-            self.wall_list.append(wall)
-
-
-
-        # Put some crates on the ground
-        # This shows using a coordinate list to place sprites
-        coordinate_list = [[512, 96], [256, 96], [768, 96]]
-
-        for coordinate in coordinate_list:
-            # Add a crate on the ground
-            wall = arcade.Sprite(
-                ":resources:images/tiles/boxCrate_double.png", scale=TILE_SCALING
-            )
-            wall.position = coordinate
-            self.wall_list.append(wall)
+        self.player_list.append(self.ball)
+        self.score = 0
+        self.dead = False
 
         # Create a Simple Physics Engine, this will handle moving our
         # player as well as collisions between the player sprite and
         # whatever SpriteList we specify for the walls.
         self.physics_engine = arcade.PhysicsEngineSimple(
-            self.player_sprite, self.wall_list
+            self.player_sprite
         )
-        self.background_color = arcade.csscolor.CORNFLOWER_BLUE
-
-    def setup(self):
-        """Set up the game here. Call this function to restart the game."""
-        pass
 
     def on_draw(self):
         """Render the screen."""
 
         # Clear the screen to the background color
         self.clear()
+        
+        #die
+        if self.dead:
+            arcade.draw_text("you lose", WINDOW_WIDTH/2, WINDOW_HEIGHT/2, arcade.color.WHITE, 80)    
+            self.score = 0
+
 
         # Draw our sprites
         self.player_list.draw()
-        self.wall_list.draw()
+        output = "Score: " + str(self.score)
+        arcade.draw_text(output, 1000, WINDOW_HEIGHT-30, arcade.color.WHITE, 24)
+
+
+
 
 
     def on_update(self, delta_time):
         """Movement and Game Logic"""
         # Move the player using our physics engine
         self.physics_engine.update()
+
+        #check bounds  
+
+        if self.player_sprite.center_y > WINDOW_HEIGHT/2:
+            self.player_sprite.center_y = WINDOW_HEIGHT/2
+
+        # Check   hits
+        hitshit = arcade.check_for_collision(self.player_sprite, self.ball)
+        if hitshit:
+            self.score +=1
+            self.ball.change_y = self.ball.change_y*-1
+            self.ball.forward(5)
+
+
+        # move terrence
+        self.ball.center_x += self.ball.change_x
+        self.ball.center_y += self.ball.change_y
+        self.ball.angle = (math.atan((self.ball.change_y/self.ball.change_x)))*180/3.14
+
+
+        # bounce terance
+        if self.ball.left < 0 or self.ball.right > WINDOW_WIDTH:
+            self.ball.change_x *= -1  # Reverse horizontal direction
+        if self.ball.bottom < 0 or self.ball.top > WINDOW_HEIGHT:
+            self.ball.change_y *= -1  # Reverse vertical direction
+
+        #  die
+        
+        #die
+        if self.ball.bottom < 1:
+            self.dead = True
+        
+
+            
+
+
 
     def on_mouse_motion(self, x, y, dx, dy):
 
